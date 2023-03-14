86b54619b3ae8c5407d09279932147401166dcd3
olender <olender@usp.br>
2023-03-14 18:09:17 -0300

    DOF count for marmousi cases

---
+++ get_dofs_marmousi_no_PML.py
@@ -0,0 +1,53 @@
+from firedrake import File
+import spyro
+
+model = {}
+
+model["opts"] = {
+    "method": "KMV",  # either CG or KMV
+    "quadrature": "KMV",  # Equi or KMV
+    "degree": 4,  # p order
+    "dimension": 2,  # dimension
+}
+model["parallelism"] = {
+    "type": "automatic",
+}
+model["mesh"] = {
+    "Lz": 3.5,  # depth in km - always positive
+    "Lx": 17.0,  # width in km - always positive
+    "Ly": 0.0,  # thickness in km - always positive
+    "meshfile": "meshes/marmousi_no_pml.msh",
+    "initmodel": None,
+    "truemodel": "velocity_models/marmousi_no_pml.hdf5",
+}
+model["BCs"] = {
+    "status": False,  # True or false
+    "outer_bc": "non-reflective",  # None or non-reflective (outer boundary condition)
+    "damping_type": "polynomial",  # polynomial, hyperbolic, shifted_hyperbolic
+    "exponent": 2,  # damping layer has a exponent variation
+    "cmax": 4.5,  # maximum acoustic wave velocity in PML - km/s
+    "R": 1e-6,  # theoretical reflection coefficient
+    "lz": 0.0,  # thickness of the PML in the z-direction (km) - always positive
+    "lx": 0.0,  # thickness of the PML in the x-direction (km) - always positive
+    "ly": 0.0,  # thickness of the PML in the y-direction (km) - always positive
+}
+model["acquisition"] = {
+    "source_type": "Ricker",
+    "num_sources": 40,
+    "source_pos": spyro.create_transect((-0.01, 1.0), (-0.01, 15.0), 40),
+    "frequency": 5.0,
+    "delay": 1.0,
+    "num_receivers": 500,
+    "receiver_locations": spyro.create_transect((-0.10, 0.1), (-0.10, 17.0), 500),
+}
+model["timeaxis"] = {
+    "t0": 0.0,  # Initial time for event
+    "tf": 5.00,  # Final time for event
+    "dt": 0.00025,
+    "amplitude": 1,  # the Ricker has an amplitude of 1.
+    "nspool": 100,  # how frequently to output solution to pvds
+    "fspool": 99999,  # how frequently to save solution to RAM
+}
+comm = spyro.utils.mpi_init(model)
+mesh, V = spyro.io.read_mesh(model, comm)
+print(f"Dofs count = {V.dim()} without PML")
